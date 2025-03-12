2668ceb0ce3a35e1ecc377e8f5a1fdf87a373bf8
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-12 15:16:39 +0530

    ToString

---
+++ Spring/src/main/java/has_a_relationship/Person.java
@@ -0,0 +1,59 @@
+package has_a_relationship;
+
+public class Person {
+	
+	private int id;
+	private String name;
+	private String gender;
+	private int age;
+	
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", gender=" + gender + ", age=" + age + ", address=" + address
+				+ "]";
+	}
+	
+
+}
