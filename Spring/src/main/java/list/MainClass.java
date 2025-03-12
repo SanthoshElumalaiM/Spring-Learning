bdb9cde682f669f5e0f47e6b63e9e226c241e66b
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-12 16:22:20 +0530

    getBean

---
+++ Spring/src/main/java/list/MainClass.java
@@ -0,0 +1,14 @@
+package list;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("listConfig.xml");
+		
+		Order o=(Order)context.getBean("o1");
+		System.out.println(o);
+	}
+
+}
