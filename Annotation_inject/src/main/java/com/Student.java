fb31d394d370ab40f83ac1d252f2048d90a1e64c
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-14 15:53:47 +0530

    Student

---
+++ Annotation_inject/src/main/java/com/Student.java
@@ -0,0 +1,11 @@
+package com;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	Student() {
+		System.out.println("Student constructor is called");
+	}
+
+}
