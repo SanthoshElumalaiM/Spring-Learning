47046e9453587c00d82d36238256a3df779ab234
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-15 15:20:33 +0530

    getBean("person")

--- Annotation_inject/src/main/java/has_a_relationship/MainClass.java
+++ Annotation_inject/src/main/java/has_a_relationship/MainClass.java
@@ -1,13 +1,14 @@
 package has_a_relationship;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		ApplicationContext context=new ClassPathXmlApplicationContext("HasARelConfig.xml");
-		Person p=(Person)context.getBean("p1");
+		ApplicationContext context=new AnnotationConfigApplicationContext(PersonConfig.class);
+		Person p=(Person)context.getBean("person");
 		System.out.println(p);
 		
 	}
