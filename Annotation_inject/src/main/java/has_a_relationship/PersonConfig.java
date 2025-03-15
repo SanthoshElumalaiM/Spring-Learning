90058c80e20ecc8cd9c84b8907988ab33a656b68
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-15 15:19:50 +0530

    PerosnConfig

---
+++ Annotation_inject/src/main/java/has_a_relationship/PersonConfig.java
@@ -0,0 +1,11 @@
+package has_a_relationship;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "has_a_relationship")
+public class PersonConfig {
+
+}
