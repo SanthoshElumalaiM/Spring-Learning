d1bc3c65b614158eabdc3e756becc97e30cf636d
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-15 15:20:03 +0530

    Person

--- Annotation_inject/src/main/java/has_a_relationship/Person.java
+++ Annotation_inject/src/main/java/has_a_relationship/Person.java
@@ -1,12 +1,22 @@
 package has_a_relationship;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("person")
 public class Person {
 	
+	@Value("1")
 	private int id;
+	@Value("Hartik")
 	private String name;
+	@Value("Male")
 	private String gender;
+	@Value("30")
 	private int age;
 	
+	@Autowired//it is used to inject the Address ref
 	private Address address;
 
 	public int getId() {
