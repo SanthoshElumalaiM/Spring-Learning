4104feef375f6e16bb64f33878dd084e35a821c1
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-17 15:27:22 +0530

    Qualifier("dog")

--- Annotation_inject/src/main/java/has_a_relationship/Dog.java
+++ Annotation_inject/src/main/java/has_a_relationship/Dog.java
@@ -1,8 +1,11 @@
 package has_a_relationship;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("d")
+//@Primary
+@Qualifier("dog")
 public class Dog  implements Animal{
 	
 	public void sound()
