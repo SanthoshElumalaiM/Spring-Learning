6467d74a99930fe43dc5976a49102cb470c8b3b7
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-17 14:54:32 +0530

    MainClass

---
+++ Annotation_inject/src/main/java/has_a_relationship/ZooMainClass.java
@@ -0,0 +1,16 @@
+package has_a_relationship;
+
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import io.micrometer.observation.Observation.Context;
+
+public class ZooMainClass {
+
+	ApplicationContext context=new AnnotationConfigApplicationContext(ZooConfig.class);
+
+	con
+	
+	
+}
