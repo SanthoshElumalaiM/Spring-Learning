941d0b22c840404dd2d4e81e55f2169bb204779d
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-17 15:27:07 +0530

    Qualifier("lion")

--- Annotation_inject/src/main/java/has_a_relationship/Zoo.java
+++ Annotation_inject/src/main/java/has_a_relationship/Zoo.java
@@ -1,16 +1,23 @@
 package has_a_relationship;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("z")
 public class Zoo {
 	
 	@Autowired
-	Animal animal;
+	@Qualifier("dog")
+	Animal animal1;//new dog()
+	
+	@Autowired
+	@Qualifier("lion")
+	Animal animal2;//new lion()
 	
 	public void getAnimalSound() {
-		animal.sound();
+		animal1.sound();
+		animal2.sound();
 		
 		
 	}
