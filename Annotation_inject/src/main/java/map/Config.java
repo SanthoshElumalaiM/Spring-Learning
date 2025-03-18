7d141abcd00f101a9fc0d8f6446690420d7f4fcb
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-18 16:21:08 +0530

    Map<Integer,String>

---
+++ Annotation_inject/src/main/java/map/Config.java
@@ -0,0 +1,46 @@
+package map;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan///it will consider config class which package
+public class Config {
+
+	@Bean
+	@Qualifier("m1")
+	public Map<Integer,String> m1(){
+		
+		LinkedHashMap<Integer,String> al=new LinkedHashMap<Integer,String>();
+		
+		al.put(1,"Hartik");
+		al.put(2,"Rohit");
+		al.put(3,"surya");
+		al.put(4,"kunal");
+		
+		
+		return al;
+		
+	}
+	@Bean
+//	@Primary
+	@Qualifier("m2")
+
+	public Map<Integer,String> m2(){
+		
+		LinkedHashMap<Integer,String> al=new LinkedHashMap<Integer,String>();
+		
+		al.put(5, "bumrah");
+		al.put(6, "ishan");
+		al.put(7, "boult");
+		return al;
+		
+	}
+}
