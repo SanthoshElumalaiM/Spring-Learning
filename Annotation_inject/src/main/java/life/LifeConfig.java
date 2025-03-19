d5a8d8fb70027976d70ff90187d30452903c7d61
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-19 15:38:48 +0530

    getBean

---
+++ Annotation_inject/src/main/java/life/LifeConfig.java
@@ -0,0 +1,11 @@
+package life;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan
+public class LifeConfig {
+
+}
