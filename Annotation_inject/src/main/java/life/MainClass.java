e0712f8056bf949722dc0e89c83e37013d74a9ec
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-20 16:50:07 +0530

    mainClass

--- Annotation_inject/src/main/java/life/MainClass.java
+++ Annotation_inject/src/main/java/life/MainClass.java
@@ -7,9 +7,14 @@ public class MainClass {
 
 	public static void main(String[] args) 
 	{
+		
 		 ApplicationContext context=new AnnotationConfigApplicationContext(LifeConfig.class);
 		 
 		 LifeCycle l=(LifeCycle)context.getBean("lifeCycle");
+		 LifeCycle l1=(LifeCycle)context.getBean("lifeCycle");
 		 l.m3();
+		 System.out.println(l);
+		 System.out.println(l1);
+		 
 	}
 }
